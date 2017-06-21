3847310deb7d15558678b6e9de3467150673ab97
Mariel Segovia <msegovia94@gmail.com>
2017-06-21 05:24:14 +0000

    added edit route to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,4 +34,27 @@ class BooksController < ApplicationController
     end
   end
 
+  get '/books/:id/edit' do
+    if logged_in?
+      @book = Book.find_by_id(params[:id])
+      if @book.user_id == current_user.id
+        erb :'books/edit'
+      else
+        redirect to '/books'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/books/:id' do
+    @book = Book.find_by_id(params[:id])
+    if params[:title].empty? || params[:author].empty?
+      redirect to "/books/#{@book.id}/edit"
+    else
+      @book.update(:title => params[:title], :author => params[:author])
+      redirect to "/books/#{@book.id}"
+    end
+  end
+
 end
