4520240524dae740e285331855456a118352b6e7
Mariel Segovia <msegovia94@gmail.com>
2017-06-21 04:54:29 +0000

    included s/rack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,26 @@
 require './config/environment'
 
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
+
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
   end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 
 end
