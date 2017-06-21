f4bebe0edb6b23782f5a905315a468dc3e28b91d
Mariel Segovia <msegovia94@gmail.com>
2017-06-21 04:59:19 +0000

    slugi route in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  
+
   get '/' do
     erb :'index'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
 end
