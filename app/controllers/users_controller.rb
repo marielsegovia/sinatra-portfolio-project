63d848bad728a42f15a74942e38c71fe5dc21959
Mariel Segovia <msegovia94@gmail.com>
2017-06-21 05:03:05 +0000

    created CREATE route- get & post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,23 @@ class UsersController < ApplicationController
     erb :'users/show'
   end
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/books'
+    else
+      erb :'users/new'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/books'
+    end
+  end
+
 end
